ba3af8f34636ef940fb8929a3fb90eeab4f45409
LeoTosqo <leotosqo@gmail.com>
2025-05-22 04:25:53 -0700

    update

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ const userSchema = new Schema(
     },
     role: {
       type: String,
-      enum: ["tenant", "landLOrd"],
+      enum: ["tenant", "landlord"],
       default: "tenant",
     },
     password: {

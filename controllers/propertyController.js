8b12d5cdc7bc6acf1b39782bdfadd72dcfb38666
LeoTosqo <leotosqo@gmail.com>
2025-06-02 03:13:03 -0700

    update properpertycontroller

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -89,6 +89,7 @@ const getAllProperties = async (req, res) => {
       totalPages,
       currentPage: parseInt(page),
       properties,
+      totalProperties,
     });
   } catch (error) {
     console.error(error);
